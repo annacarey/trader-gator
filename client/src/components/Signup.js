ccc948d5be26ef7d30a78444d536e3680f4b6979
github username <annacarey@icloud.com>
2020-05-21 19:55:14 -0600

    created controlled signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
- function Signup() {  
+class Signup extends React.Component {  
 
-    return (
-        <div>
-            Signup
+    state = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+    }
 
-        </div>
-    )
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log("submit worked")
+    }
+
+    render () {
+        return (
+            <div>
+                Signup
+                <form onSubmit = {this.handleSubmit}>
+                    <input onChange={e => this.setState({firstName: e.target.value})} type="text" name="firstName" placeholder="First name..." />
+                    <input onChange={e => this.setState({lastName: e.target.value})} type="text" name="lastName" placeholder="Last name..." />
+                    <input onChange={e => this.setState({email: e.target.value})} type="text" name="email" placeholder="Last name..." />
+                    <input onChange={e => this.setState({password: e.target.value})} type="password" name="password" placeholder="Password..." />
+                    <input onChange={e => this.setState({passwordConfirmation: e.target.value})} type="password" name="password" placeholder="Confirm password..." />
+                    <input type="submit" value="Signup" />
+                </form>
+            </div>
+        )
+    }
+    
 }
 
 export default Signup
