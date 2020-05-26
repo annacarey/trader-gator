b9a420d04e8184fc729032b9a6d39dc397599042
github username <annacarey@icloud.com>
2020-05-25 20:51:40 -0600

    styled navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {NavLink, withRouter} from "react-router-dom";
 import {logoutActionCreator} from '../actionCreators'
+import styled from 'styled-components'
 
 function Navigation(props) {  
 
@@ -10,11 +11,11 @@ function Navigation(props) {
     }
 
     return (
-        <div>
-            <NavLink to="/portfolio" exact>Portfolio</NavLink>
-            <NavLink to="/transactions" exact>Transactions</NavLink>
-            <button onClick={handleClick}>Logout</button>
-        </div>
+        <Wrapper>
+            <Option><MenuLink to="/portfolio" exact>Portfolio</MenuLink></Option>
+            <Option><MenuLink to="/transactions" exact>Transactions</MenuLink></Option>
+            <LogoutLink onClick={handleClick}>Logout</LogoutLink>
+        </Wrapper>
     )
 }
 
@@ -25,3 +26,40 @@ const mdp = dispatch => {
 }
 
 export default withRouter(connect(null, mdp)(Navigation))
+
+const Wrapper = styled.div`
+    display: flex;
+    font-size: 12px;
+    justify-content: center;
+`
+const Option = styled.div`
+    cursor: pointer;
+    padding: 8px;
+    padding-left: 5px;
+    padding-right: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+
+const MenuLink = styled(NavLink)`
+    color: black;
+    padding: 0px;
+    text-decoration: none;
+    &:visited {
+        color: black;
+    }
+    &:hover {
+        color: #A9A9A9;
+    }
+`
+
+const LogoutLink = styled.button`
+    border: none;
+    padding: 0px;
+    cursor: pointer;
+    font-size: 12px;
+    &:hover {
+        color: #A9A9A9;
+    }
+`
