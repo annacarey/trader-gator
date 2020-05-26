6ab3e90c2c6faed25a97bd33ad2f9c53741128c8
github username <annacarey@icloud.com>
2020-05-26 15:54:50 -0600

    added endpoint info to iex controller

--- app/controllers/iex_client_wrapper_controller.rb
+++ app/controllers/iex_client_wrapper_controller.rb
@@ -1,6 +1,7 @@
 class IexClientWrapperController < ApplicationController
 
     # Initial the Ruby IEX client - documentation here: https://github.com/dblock/iex-ruby-client
+    # Currently using sandbox, switch endpoint to 'https://cloud.iexapis.com/stable' for real data; need a paid account to access methods
     def client 
         client = IEX::Api::Client.new(
             publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
